99bde6e145c918d0876072a74302ca23ae3ec396
DAVID MORTON - STUDENT <G00351634@gmit.ie>
2019-09-30 11:50:22 +0100

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {MovieserviceService} from './Services/movieservice.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent{
   title = 'movie-app';
+MyMovies: any = [];
+  constructor(private movieservice:MovieserviceService){
+    this.movieservice.GetMovieInformation().subscribe((data)=>{
+
+      this.MyMovies = data.Search;
+      console.log(this.MyMovies);
+
+})
+  
+
+}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import {HttpClientModule} from '@angular/common/http';
+
+import {
+  MatInputModule,
+  MatCardModule,
+  MatButtonModule,
+  MatToolbarModule,
+  MatExpansionModule
+  } from '@angular/material';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+MatCardModule,
+MatButtonModule,
+MatToolbarModule,
+MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
